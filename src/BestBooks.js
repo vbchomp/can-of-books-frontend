be47cdbe07cafebcc466553423ad863cb298d5ab
Heather B <vbchomp@gmail.com>
2021-08-15 17:29:53 -0700

    and a big fat nothing for about 3 hours of working. I could not get my background image to work. Although I was able to get the home and profile links to move a little apart. I lost my login button while trying to move it into the header. I am now failing to compile since i added the token claim from in class demo and i'm calling it a day.

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,16 +2,30 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
+import patricktomasso from './patricktomasso.JPEG';
 
 class MyFavoriteBooks extends React.Component {
   render() {
-    return(
-      <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
-      </Jumbotron>
+    return (
+      <>
+        <Jumbotron>
+          <h1>My Favorite Books</h1>
+          <div className="shape shape-style-1 shape-default alpha-4">
+            <span>
+              <img
+                style={{ height: 'auto', width: '100%' }}
+                src={patricktomasso}
+                alt="Open Book Lot by Patrick Tomasso"
+                title="Open Book Lot by Patrick Tomasso"
+                description="Open Book Lot by Patrick Tomasso"
+              />
+            </span>
+          </div>
+          <p>
+            This is a collection of my favorite books
+          </p>
+        </Jumbotron>
+      </>
     )
   }
 }
--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
 import { Link } from "react-router-dom";
 import './Header.css';
+import { useAuth0 } from "@auth0/auth0-react";
 
 class Header extends React.Component {
   render() {
-    return(
+    const { isAuthenticated, isLoading } = useAuth0();
+
+    if (isLoading) {
+      return <div>Loading ...</div>;
+    }
+    return (
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
-        {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        {/* DONE: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        {isAuthenticated ?
+          <LogoutButton /> :
+          <LoginButton
+          />}
       </Navbar>
     );
   }
