d8831304ef2ae6414f967d7dcdd8cf3a22b6e465
Heather B <vbchomp@gmail.com>
2021-08-26 22:47:11 -0700

    i am done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -3,13 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import './BestBooks.css';
 import patricktomasso from './img/patricktomasso.JPEG';
-// import library from './img/library.png';
 import { withAuth0 } from "@auth0/auth0-react";
 import axios from 'axios';
 import Carousel from 'react-bootstrap/Carousel'
 import Button from 'react-bootstrap/Button'
 import BookFormModal from './BookFormModal';
 import BookUpdateFormModal from './BookUpdateFormModal';
+
+const SERVER = process.env.REACT_APP_BACKEND_URL;
 
 class MyFavoriteBooks extends React.Component {
   constructor(props) {
@@ -36,7 +37,7 @@ class MyFavoriteBooks extends React.Component {
       params: { email: this.props.auth0.user.email },
     };
     console.log('email:', email);
-    const results = await axios.get('http://localhost:3001/books', config);
+    const results = await axios.get(`${SERVER}/books`, config);
     console.log(results.data);
     this.setState({
       books: results.data,
@@ -54,14 +55,14 @@ class MyFavoriteBooks extends React.Component {
       // params sending the email from auth0 to the database request on server side
       params: { email: this.props.auth0.user.email },
     };
-    const serverResponse = await axios.get('http://localhost:3001/test', config);
+    const serverResponse = await axios.get(`${SERVER}/test`, config);
     console.log('I hope it works this way, since it did not work the other way', serverResponse);
   }
 
   // 
   handleCreate = async (book) => {
     try {
-      let response = await axios.post('http://localhost:3001/books', book);
+      let response = await axios.post(`${SERVER}/books`, book);
       console.log('i am here');
       console.log(response.data);
       this.setState({
@@ -75,7 +76,7 @@ class MyFavoriteBooks extends React.Component {
 
   handleDelete = async (id) => {
     try {
-      let response = await axios.delete(`http://localhost:3001/books/${id}`);
+      let response = await axios.delete(`${SERVER}/books/${id}`);
       console.log('i am here');
       console.log('response:', response.data);
       let remainingBooks = this.state.books.filter(book => book._id !== id);
@@ -91,7 +92,7 @@ class MyFavoriteBooks extends React.Component {
 
   handleUpdate = async (book) => {
     try {
-      let response = await axios.put(`http://localhost:3001/books/${book._id}`, book);
+      let response = await axios.put(`${SERVER}/books/${book._id}`, book);
       console.log('i am here');
       console.log('response:', response.data);
       let updatingBooks = this.state.books.map(stateBook => {
@@ -147,59 +148,55 @@ class MyFavoriteBooks extends React.Component {
       <>
         <Jumbotron>
           <h1>My Favorite Books</h1>
-          {/* <div className="shape shape-style-1 shape-default alpha-4">
-            <span>
-              <img
-                style={{ height: 'auto', width: '100%' }}
-                src={patricktomasso}
-                alt="Open Book Lot by Patrick Tomasso"
-                title="Open Book Lot by Patrick Tomasso"
-                description="Open Book Lot by Patrick Tomasso"
-                // src={library}
-                // title="library icon by Smashicons and Flaticons"
-                // alt="library icon available here https://www.flaticon.com/authors/smashicons"
-              />
-            </span>
-          </div> */}
           <p>
             This is a collection of my favorite books
           </p>
-          {this.state.books.length > 0 ? <Carousel> {this.state.books.map(book => (
-            <Carousel.Item key={book._id}>
-              <img
-                className="d-block w-100"
-                style={{ height: 'auto', width: '100%' }}
-                src={patricktomasso}
-                alt="Open Book Lot by Patrick Tomasso"
-                title="Open Book Lot by Patrick Tomasso"
-                description="Open Book Lot by Patrick Tomasso"
-              />
-              <Carousel.Caption>
-                <h3>{book.title}</h3>
-                <p>{book.description}</p>
-                <p>{book.status}</p>
-              </Carousel.Caption>
-              <Button
-                variant="outline-danger"
-                className="randomDeleteButton"
-                onClick={() => this.handleDelete(book._id)}>Don't Touch
-              </Button>
-              <Button
-                variant="outline-danger"
-                className="randomUpdateButton"
-                onClick={() => this.openUpdateModal(book)}>Make Changes to a Book?</Button>
-            </Carousel.Item>
-          ))}
-          </Carousel> : ''}
+          {
+            this.state.books.length > 0 ?
+              <Carousel>
+                {this.state.books.map(book => (
+                  <Carousel.Item key={book._id}>
+                    <img
+                      className="d-block w-100"
+                      style={{ height: 'auto', width: '100%' }}
+                      src={patricktomasso}
+                      alt="Open Book Lot by Patrick Tomasso"
+                      title="Open Book Lot by Patrick Tomasso"
+                      description="Open Book Lot by Patrick Tomasso"
+                    />
+                    <Carousel.Caption>
+                      <h3>{book.title}</h3>
+                      <p>{book.description}</p>
+                      <p>{book.status}</p>
+                    </Carousel.Caption>
+                    <Button
+                      variant="outline-danger"
+                      className="randomDeleteButton"
+                      onClick={() => this.handleDelete(book._id)}>Don't Touch
+                    </Button>
+                    <Button
+                      variant="outline-secondary"
+                      className="randomUpdateButton"
+                      onClick={() => this.openUpdateModal(book)}>Make Changes to a Book?</Button>
+                  </Carousel.Item>
+                ))}
+              </Carousel> : ''}
           <button onClick={this.openModal}>Want to Add a New Book?</button>
-          {/* <button onClick={this.openModal}>Make Changes to a Book?</button> */}
-          <BookFormModal isOpen={this.state.isOpen} closeModal={this.closeModal} handleCreate={this.handleCreate}
+          <BookFormModal
+            isOpen={this.state.isOpen}
+            closeModal={this.closeModal}
+            handleCreate={this.handleCreate}
           />
-          {this.state.isUpdateOpen ? <BookUpdateFormModal isUpdateOpen={this.state.isUpdateOpen} closeUpdateModal={this.closeUpdateModal} handleUpdate={this.handleUpdate} selectedBook={this.state.selectedBook}
-          /> : ''}
-          {/* {this.state.isOpen} ? <BookFormModal /> : ''; */}
-          {/* <button onClick={this.makeAuthReq}>Take me to your Server</button>
-          <p>Check the console!</p> */}
+          {
+            this.state.isUpdateOpen ?
+              <BookUpdateFormModal
+                isUpdateOpen={this.state.isUpdateOpen}
+                closeUpdateModal={this.closeUpdateModal}
+                handleUpdate={this.handleUpdate}
+                selectedBook={this.state.selectedBook}
+              />
+              : ''
+          }
         </Jumbotron>
       </>
     )
